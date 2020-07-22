c454cbf2a93ec316dbb2fc1697cae939eb6fe89a
Bez Worcou <beazaworcou@gmail.com>
2020-07-22 14:40:49 +1000

    add slidetoggle jquery textbox onclick

---
+++ week-five/beaziyt/js.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', ready)
+//
+// function ready() {
+//   var a = document.querySelector('.a')
+//
+//   function buttonClicked() {
+//     if(a.classList.contains('clicked')) {
+//       b.classList.remove('clicked')
+//     } else {
+//       apple.classList.add('clicked')
+//     }
+//   }
+//
+//   a.addEventListener('click', aClicked)
+// }
